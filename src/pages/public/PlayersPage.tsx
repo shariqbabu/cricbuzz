ad7cee156817e56c7536f34ac7a942fcff5b1b72
shariqbabu <36099197+shariqbabu@users.noreply.github.com>
2026-06-03 17:15:15 +0530

    Update PlayersPage.tsx

--- src/pages/public/PlayersPage.tsx
+++ src/pages/public/PlayersPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Layout } from '@/components/common/Layout';
+import Layout from '@/components/common/Layout';
 import { usePlayers } from '@/hooks/useTeamPlayer';
 import { User, Search } from 'lucide-react';
 
